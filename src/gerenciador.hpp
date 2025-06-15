b7f77b274ed4089aa92b7e339b120a1b2bf358b7
Hugo Enrique Santana Keles <167632489+hugokeles@users.noreply.github.com>
2025-06-14 22:04:08 -0300

    Update gerenciador.hpp

--- src/gerenciador.hpp
+++ src/gerenciador.hpp
@@ -1,1 +1,26 @@
+#ifndef GERENCIADOR_HPP
+#define GERENCIADOR_HPP
 
+#include <vector>
+
+#include "tarefa.hpp"
+
+class GerenciadorTarefas {
+private:
+  std::vector<Tareda> tarefas;
+
+public:
+   // Método para adicionar tarefas
+  void adicionarTarefa(const Tarefa& tarefa);
+
+  // Método para remover por índice
+  void removerTarefa(int indice);
+
+  //Método para listar tarefas
+  void listarTarefas(bool apenasPendentes = false) const;
+
+  //Filtra tarefas por disciplina
+  std::vector<Tarefa> filtrarPorDisciplina(cont std::string& disciplina) const;
+}
+#endif
+  
